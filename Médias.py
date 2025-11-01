fed5cd6993e6b20c9ab6ba71ed792ecdcd2409fc
GustavoSilva11620 <shark11620@sharkcoders.pt>
2025-11-01 20:54:30 +0000

    Médias.py

--- Médias.py
+++ Médias.py
@@ -5,4 +5,7 @@ mediadanotastopsenegativas=(nota1+nota2)/2
 if mediadanotastopsenegativas <=60:
     print("man tiraste negativa :(")
 else:
+
     print("TIRASTE POSITIVA BOAAA XD XD XD :D :D D: ")
+    end
+end
